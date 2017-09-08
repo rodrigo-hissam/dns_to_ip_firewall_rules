8ac3a6177674853c3731ef9e5face822155820a1
ROMO <rodrigo.hissam@gmail.com>
2017-09-08 15:54:46 -0500

    Adding the ability to set custom ports and protocol per domain

--- dns-to-ip-firewall-rules.py
+++ dns-to-ip-firewall-rules.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 """Dynamic dns to ip address firewall rule creator.
 
 Resolve dynamic dns names into ips and automatically create/destroy
@@ -45,29 +45,65 @@ from platform import linux_distribution
 def main():
     """Main entry point for the script."""
     # Variables
-    # Example domain names, use your set your configured dynamic dns names here
-    dynamic_domains = ["mangolassi.it", "google.com", "theverge.com"]
+    #
+    # Config your domains their ports and the protocol needed per port. If you
+    # want both udp and tcp protocols for the port enter 'both' for protocol
+    # type.
+    # Example: domain with its ports and protocol:
+    #   {
+    #    'name': 'example.com',
+    #    'ports': [
+    #       (53, 'udp'),
+    #       (22, 'both'),
+    #       (80, 'tcp')
+    #       ]
+    #   },
+    dynamic_domains = [
+        {
+         'name': 'google.com',
+         'ports': [
+            (53, 'udp'),
+            (22, 'both'),
+            (80, 'tcp')
+            ]
+        },
+        {
+         'name': 'example.com',
+         'ports': [
+            (53, 'udp'),
+            (22, 'both'),
+            (80, 'tcp')
+            ]
+        },
+        {
+         'name': 'mangolassi.it',
+         'ports': [
+            (53, 'both'),
+            (443, 'tcp')
+         ]
+        }
+    ]
+    # Getting linux distro
     distro = linux_distribution()[0]
-
     # Script start
     for domain in dynamic_domains:
-        current_ip = get_current_ip(domain)
-        if os.path.isfile(domain):
-            old_ip = get_logged_ip(domain)
+        current_ip = get_current_ip(domain['name'])
+        if os.path.isfile(domain['name']):
+            old_ip = get_logged_ip(domain['name'])
             if not current_ip == old_ip:
-                delete_firewall_rule(distro, old_ip)
-                create_firewall_rule(distro, current_ip)
+                delete_firewall_rule(distro, old_ip, domain['ports'])
+                create_firewall_rule(distro, current_ip, domain['ports'])
                 print("\nAdding {} ip {} - removing {}".format
-                      (domain, current_ip, old_ip))
-                create_hostname_ip_log(domain, current_ip)
+                      (domain['name'], current_ip, old_ip))
+                create_hostname_ip_log(domain['name'], current_ip)
             else:
                 print("\nSame ip address nothing to do")
         else:
-            create_hostname_ip_log(domain, current_ip)
-            create_firewall_rule(distro, current_ip)
+            create_hostname_ip_log(domain['name'], current_ip)
+            create_firewall_rule(distro, current_ip, domain['ports'])
             print("\nAdding to firewall")
 
-        print("{0} - {1}".format(domain, current_ip))
+        print("{0} - {1}".format(domain['name'], current_ip))
     print("\n")
 
 
@@ -96,24 +132,38 @@ def get_logged_ip(domain):
     return logged_ip
 
 
-def create_firewall_rule(distro, ip):
+def create_firewall_rule(distro, ip, ports):
     """Create firewall rule based on newest ip of dynamic domain."""
     if 'Ubuntu' in distro:
-        Popen(["ufw", "allow", "from", ip, "to", "any", "port", "53"],
-              stdout=PIPE, stderr=PIPE)
-        # ufw freaks out when adding rules too fast
-        time.sleep(1)
+        for port in ports:
+            if port[1] == 'both':
+                Popen(
+                 ["ufw", "allow", "from", ip, "to", "any", "port",
+                 str(port[0])], stdout=PIPE, stderr=PIPE)
+            else:
+                Popen(
+                  ["ufw", "allow", "from", ip, "to", "any", "port",
+                   str(port[0]), "proto", port[1]], stdout=PIPE, stderr=PIPE)
+            # ufw freaks out when adding rules too fast
+            time.sleep(.5)
     elif 'Cent' in distro or 'Fedora' in distro or 'Red' in distro:
         print("")
 
 
-def delete_firewall_rule(distro, ip):
+def delete_firewall_rule(distro, ip, ports):
     """Delete firewall rule in order to add new ip from dynamic domain."""
     if 'Ubuntu' in distro:
-        Popen(["ufw", "delete", "allow", "from", ip, "to", "any", "port",
-              "53"], stdout=PIPE, stderr=PIPE)
-        # ufw freaks out when deleting rules too fast
-        time.sleep(1)
+        for port in ports:
+            if port[1] == 'both':
+                Popen(
+                 ["ufw", "delete", "allow", "from", ip, "to", "any", "port",
+                  str(port[0])], stdout=PIPE, stderr=PIPE)
+            else:
+                Popen(
+                  ["ufw", "delete", "allow", "from", ip, "to", "any", "port",
+                   str(port[0]), "proto", port[1]], stdout=PIPE, stderr=PIPE)
+            # ufw freaks out when deleting rules too fast
+            time.sleep(.5)
     elif 'Cent' in distro or 'Fed' in distro or 'Red' in distro:
         print("")
 
